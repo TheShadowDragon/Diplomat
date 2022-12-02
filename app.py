2c2aef25a5ab613b949b38865f818d106d7f1e72
Best Dev <63375256+TheShadowDragon@users.noreply.github.com>
2022-12-02 06:19:27 -0600

    registration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+'''
+<a href=#^user^ style="size: 10px;">^user^</a>
+'''
 import json
 from time import time
-import cryptocode
 from flask import (
     Flask,
     abort,
@@ -10,17 +12,30 @@ from flask import (
     send_file,
     send_from_directory,
     url_for,
-)
-from werkzeug.datastructures import ImmutableMultiDict
-from flask import session
-from random import randrange
 
+)
+#pip install Flask-login
+from flask_login import current_user, LoginManager, login_user, logout_user, login_required, UserMixin
+from flask import session
+import random
+from flask import Flask, request, jsonify
+from datetime import datetime
+#pip install email_validator
+
+global display
+display = "Login/Register"
 post = open("templates/index.html", "r").read()
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return (
+        open("templates/index.html", "r")
+        .read()
+        .replace("^user^", display)
+        .replace("^webpage^", "")
+        .replace("^Title^", "Home")
+    )
 
 @app.route("/Main")
 def Main():
@@ -28,6 +43,7 @@ def Main():
             open("templates/index.html", "r")
             .read()
             .replace("^Title^", "Main")
+            .replace("^user^", display)
             .replace("^webpage^", open("templates/main.html", "r").read())
         )
 
@@ -37,8 +53,74 @@ def about():
             open("templates/index.html", "r")
             .read()
             .replace("^Title^", "About")
+            .replace("^user^", display)
             .replace("^webpage^", open("templates/about.html", "r").read())
         )
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    sessname = session.get("name", "Login")
+    form = """
+    <div class=posts>
+        <form action="/register">
+            <label for="id1">Email: </label>
+            <input type="text" id="id1" name="email">
+            <br>
+            <label for="id2">Username: </label>
+            <input type="text" id="id2" name="name">
+            <br>
+            <label for="id3">Password:  </label>
+            <input type="text" id="id3" name="password">
+            <br>
+            <label for="id4">Re-Enter Password:  </label>
+            <input type="text" id="id4" name="password_verification">
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>"""
+    get_users = json.loads(open("json/users.json", "r").read())
+    email = request.args.get("email")
+    name = request.args.get("name")
+    password = request.args.get("password")
+    password_verification = request.args.get("password_verification")
+    
+    if name == None or password == None or email == None or password_verification == None:
+        return (
+                open("templates/index.html", "r")
+                .read()
+                .replace("^Title^", "Register/Login")
+                .replace("^webpage^", form)
+            )
+
+
+    for item in get_users:
+        if name == item:
+            return (
+                open("templates/index.html", 'r')
+                .read()
+                .replace("^webpage^", "User already exists." + form)
+            )
+    
+    add_user = open("json/users.json", 'w')
+    get_users.update({name: {"password": password}})
+    add_user.write(json.dumps(get_users, indent=2))
+    return (
+        open("templates/index.html", 'r')
+        .read()
+        .replace("^webpage^", open("templates/user.html", 'r').read())
+    )
+
+    
+
+@app.route("/^user^")
+def user():
+    return (
+            open("templates/index.html", "r")
+            .read()
+            .replace("^Title^", "^user^")
+            .replace("^webpage^", open("templates/user.html", "r").read())
+        )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
